d5cd31a3a37f8265a3f1d699f1139aa55472c8c5
Vladimir Kraev <vladeto83@gmail.com>
2021-01-29 15:48:57 +0200

    feat: implement header component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import './App.css'
+import React from 'react'
+import styles from './app.module.css'
+import Header from './components/header'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button>click me!!!</button>
-      </header>
+    <div className={styles.app}>
+     <Header />
     </div>
   )
 }
