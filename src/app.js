c202439ea8bbca394df62a01ff8721e376ad00e0
Vladimir Kraev <vladeto83@gmail.com>
2021-02-02 17:30:53 +0200

    fix/ app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './app.module.css'
+import Aside from './components/aside'
+import Header from './components/header'
+import Origamis from './components/origamis'
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <Header />
+      <div className={styles.container}>
+        <Aside />
+        <Origamis />
+      </div>
+    </div>
+  )
+}
+
+export default App
